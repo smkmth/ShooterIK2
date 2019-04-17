306bc9370be8f79a1f66050211a49072d199b8bd
smkmth <dandavidnoon@gmail.com>
2019-04-17 18:23:50 +0100

    added a jump
    works okay

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,22 +10,58 @@ public class Player : MonoBehaviour
 
     Vector3 flip = new Vector3(-1, 1, 1);
     public bool facingRight = true;
+    public bool running = false;
 
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpHeight;
+    public float grounddrag;
+    public float airdrag;
+
+    public bool Grounded;
+    public LayerMask Ground; 
+
+    private CapsuleCollider playerCollider;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerCollider = GetComponent<CapsuleCollider>();
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.drag = grounddrag;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Grounded = Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y, playerCollider.bounds.center.z), 0.18f, Ground);
+
+        if (Grounded)
+        {
+            playerRb.drag = grounddrag;
+            playerRb.mass = 1.0f;
+
+
+        }
+        else
+        {
+            playerRb.drag = airdrag;
+            playerRb.mass = 50.0f;
+
+        }
+        PlayerAnim.SetBool("Grounded", Grounded);
+        running = Input.GetButton("Run");
+     
+        //facing and aiming
         if (Input.mousePosition.x < (Screen.width / 2))
         {
             transform.localScale = flip;
             facingRight = false;
 
-        }else
+        }
+        else
         {
             transform.localScale = Vector3.one;
             facingRight = true;
@@ -33,15 +69,49 @@ public class Player : MonoBehaviour
         Vector3 pos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
         pos.z = 0;
         MainArmIK.transform.position = pos;
+
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        //movement 
         float inputLR = Input.GetAxis("Horizontal");
         if (inputLR != 0)
         {
-            PlayerAnim.SetBool("Walking", true);
-            transform.position += Vector3.right * inputLR * Time.deltaTime ;
+            if (running)
+            {
+
+                PlayerAnim.SetBool("Walking", false);
+                PlayerAnim.SetBool("Running", true);
+                //transform.position += Vector3.right * inputLR * Time.deltaTime * runSpeed;
+                playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * runSpeed, ForceMode.Force);
+
+            }
+            else
+            {
+                PlayerAnim.SetBool("Walking", true);
+                PlayerAnim.SetBool("Running", false);
+                playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * walkSpeed, ForceMode.Force);
+
+                //transform.position += Vector3.right * inputLR * Time.deltaTime * walkSpeed;
+            }
+
         }
         else
         {
+            PlayerAnim.SetBool("Running", false);
             PlayerAnim.SetBool("Walking", false);
+
+        }
+        //jumping
+        if (Input.GetButtonDown("Jump") && Grounded)
+        {
+            Vector3 jump = new Vector3(0, 1, 0);
+            Debug.Log("jump");
+            playerRb.AddForce(jump * jumpHeight, ForceMode.Impulse);
 
         }
 
