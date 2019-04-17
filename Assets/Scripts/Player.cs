de07da80fd98c65471f84c03f0af16a95b210b43
SmkMth <dandavidnoon@gmail.com>
2019-04-17 23:48:06 +0100

    fixed jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,27 @@ public class Player : MonoBehaviour
     public GameObject MainArmIK;
     public Animator PlayerAnim;
 
-    Vector3 flip = new Vector3(-1, 1, 1);
+    public Vector3 flip = new Vector3(-.1f, .1f, .1f);
+    public Vector3 notflipped = new Vector3(.1f, .1f, .1f);
     public bool facingRight = true;
     public bool running = false;
 
-    public float walkSpeed;
-    public float runSpeed;
+    public float BaseWalkSpeed;
+    private float walkSpeed;
+    public float BaseRunSpeed;
+    private float runSpeed;
     public float jumpHeight;
+    public float airspeed;
     public float grounddrag;
     public float airdrag;
 
+    public float maxSpeed;
+
+
     public bool Grounded;
     public LayerMask Ground; 
+    public LayerMask WeaponHit;
+    public GameObject gun;
 
     private CapsuleCollider playerCollider;
     private Rigidbody playerRb;
@@ -36,17 +45,21 @@ public class Player : MonoBehaviour
     void Update()
     {
 
-        Grounded = Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y, playerCollider.bounds.center.z), 0.18f, Ground);
+        Grounded = Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y, playerCollider.bounds.center.z), 0.01f, Ground);
 
         if (Grounded)
         {
             playerRb.drag = grounddrag;
             playerRb.mass = 1.0f;
-
-
+            walkSpeed = BaseWalkSpeed;
+            runSpeed = BaseRunSpeed;
+            
+            
         }
         else
         {
+            walkSpeed = airspeed;
+            runSpeed = airspeed;
             playerRb.drag = airdrag;
             playerRb.mass = 50.0f;
 
@@ -63,12 +76,28 @@ public class Player : MonoBehaviour
         }
         else
         {
-            transform.localScale = Vector3.one;
+            transform.localScale = notflipped;
             facingRight = true;
         }
         Vector3 pos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
         pos.z = 0;
         MainArmIK.transform.position = pos;
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            
+            Physics.Raycast(gun.transform.position, pos, out hit, 100.0f, WeaponHit);
+            Debug.DrawRay(gun.transform.position, pos, Color.yellow, 10.0f);
+            if (hit.transform != null)
+            {
+                Debug.Log("Hit");
+            }
+            
+           
+           
+
+        }
 
 
     }
@@ -79,24 +108,27 @@ public class Player : MonoBehaviour
 
         //movement 
         float inputLR = Input.GetAxis("Horizontal");
-        if (inputLR != 0)
+        if (inputLR != 0 )
         {
             if (running)
             {
 
                 PlayerAnim.SetBool("Walking", false);
                 PlayerAnim.SetBool("Running", true);
-                //transform.position += Vector3.right * inputLR * Time.deltaTime * runSpeed;
-                playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * runSpeed, ForceMode.Force);
+                if (playerRb.velocity.magnitude < maxSpeed)
+                {
+                    playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * runSpeed, ForceMode.Impulse);
+                }
 
             }
             else
             {
                 PlayerAnim.SetBool("Walking", true);
                 PlayerAnim.SetBool("Running", false);
-                playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * walkSpeed, ForceMode.Force);
-
-                //transform.position += Vector3.right * inputLR * Time.deltaTime * walkSpeed;
+                if (playerRb.velocity.magnitude < maxSpeed)
+                {
+                    playerRb.AddForce(Vector3.right * inputLR * Time.deltaTime * walkSpeed, ForceMode.Impulse);
+                }
             }
 
         }
